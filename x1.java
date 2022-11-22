b4d2114780b32bdbcdbfa9e85a41ce707b760586
Oneli-bit <74351052+Oneli-bit@users.noreply.github.com>
2022-11-23 03:40:23 +0530

    tutorial 5

---
+++ x1.java
@@ -0,0 +1,14 @@
+public class x1 {
+}
+
+class X2 extends x1 {
+    public  void foo() {
+        System.out.println("foo() called in X2");
+    }
+}
+class X3 extends X2 {
+    // overriding foo() of X2
+    public void foo() {
+        System.out.println("foo() called in X3");
+    }
+}
