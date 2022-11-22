459f0913cb7f1b3565cb44a1d239e7106003b6b5
Oneli-bit <74351052+Oneli-bit@users.noreply.github.com>
2022-11-23 03:02:09 +0530

    Tutorial 1

---
+++ Q1.java
@@ -0,0 +1,18 @@
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+
+        if (args[0]=="sinhala")
+            System.out.println("This is sinhala");
+        else
+            System.out.println("This is not sinhala");
+
+        System.out.println("Time zone:"+args[1]);
+
+        System.out.println("Welcome to Online Learning!\n");
+        System.out.println("Please virus go away!");
+
+
+    }
+}
